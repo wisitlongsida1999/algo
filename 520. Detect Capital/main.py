af35c7cc2a8e1c6f2a04fadde2187b6a55b1e6ef
wisitlongsida1999 <wisitlongsida1999@gmail.com>
2023-01-02 20:49:05 +0700

    ez

---
+++ 520. Detect Capital/main.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+
+        return word == word.upper() or word == word.lower() or word == word[0].upper()+word[1:].lower()
+        
