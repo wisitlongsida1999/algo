35ca7c872e86e5ab259858adf59fe1592676cfa1
wisitlongsida1999 <wisitlongsida1999@gmail.com>
2023-01-29 10:40:58 +0700

    justCommit

---
+++ Weekly Contest 330/6337. Count Distinct Numbers on Board/main.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def distinctIntegers(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        return n-1
